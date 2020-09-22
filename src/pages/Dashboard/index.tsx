2b7c36b5de06a7b7c15df7bfc00a9efbbb4b4afe
Guilherme Chiarelli <guilherme.hchiarelli@gmail.com>
2020-09-21 22:41:28 -0300

    implementations

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,72 @@
+import React from 'react';
+
+import { FiClock, FiPower } from 'react-icons/fi';
+import {
+  Container,
+  Header,
+  HeaderContent,
+  Profile,
+  Content,
+  Schedule,
+  Calendar,
+  NextAppointment,
+} from './styles';
+
+import logoImg from '../../assets/logo.svg';
+import { useAuth } from '../../hooks/auth';
+
+const Dashboard: React.FC = () => {
+  const { signOut, user } = useAuth();
+
+  return (
+    <Container>
+      <Header>
+        <HeaderContent>
+          <img src={logoImg} alt="GoBarber" />
+
+          <Profile>
+            <img src={user.avatar_url} alt={user.name} />
+            <div>
+              <span>Bem vindo,</span>
+              <strong>{user.name}</strong>
+            </div>
+          </Profile>
+
+          <button type="button" onClick={signOut}>
+            <FiPower />
+          </button>
+        </HeaderContent>
+      </Header>
+
+      <Content>
+        <Schedule>
+          <h1>Horários agendados</h1>
+          <p>
+            <span>Hoje</span>
+            <span>Dia 6</span>
+            <span>Segunda-feira</span>
+          </p>
+
+          <NextAppointment>
+            <strong>Atendimento a seguir</strong>
+            <div>
+              <img
+                src="https://avatars3.githubusercontent.com/u/26163777?s=460&u=e7ed7d0debfefa55181ee0dc96afad6138f45c2c&v=4"
+                alt="Guilherme Chiarelli"
+              />
+
+              <strong>Guilherme Chiarelli</strong>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+          </NextAppointment>
+          <Calendar />
+        </Schedule>
+      </Content>
+    </Container>
+  );
+};
+
+export default Dashboard;
---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,110 @@
+import React, { useRef, useCallback } from 'react';
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
+import { Form } from '@unform/web';
+import { FormHandles } from '@unform/core';
+import * as Yup from 'yup';
+import { Link, useHistory } from 'react-router-dom';
+
+import { useAuth } from '../../hooks/auth';
+import { useToast } from '../../hooks/toast';
+import getValidationErrors from '../../utils/getValidationErrors';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+import logoImg from '../../assets/logo.svg';
+
+import { Container, Content, AnimationContainer, Background } from './styles';
+
+interface SignInFormData {
+  email: string;
+  password: string;
+}
+
+const SignIn: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+
+  const { user, signIn } = useAuth();
+  const { addToast } = useToast();
+
+  const history = useHistory();
+
+  console.log(user);
+
+  const handleSubmit = useCallback(
+    async (data: SignInFormData) => {
+      try {
+        formRef.current?.setErrors({});
+
+        const schema = Yup.object().shape({
+          email: Yup.string()
+            .required('E-mail obrigatório')
+            .email('Digite um e-mail válido'),
+          password: Yup.string().required('Senha obrigatória'),
+        });
+
+        await schema.validate(data, {
+          abortEarly: false,
+        });
+
+        await signIn({
+          email: data.email,
+          password: data.password,
+        });
+
+        history.push('/dashboard');
+      } catch (error) {
+        if (error instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(error);
+
+          formRef.current?.setErrors(errors);
+
+          return;
+        }
+
+        addToast({
+          type: 'error',
+          title: 'Erro na autenticação',
+          description: 'Ocorreu um erro ao fazer login, cheque as credenciais.',
+        });
+      }
+    },
+    [signIn, addToast, history],
+  );
+
+  return (
+    <>
+      <Container>
+        <Content>
+          <AnimationContainer>
+            <img src={logoImg} alt="GoBarber" />
+
+            <Form ref={formRef} onSubmit={handleSubmit}>
+              <h1>Faça seu logon</h1>
+
+              <Input icon={FiMail} name="email" placeholder="E-mail" />
+
+              <Input
+                icon={FiLock}
+                name="password"
+                type="password"
+                placeholder="Senha"
+              />
+
+              <Button type="submit">Entrar</Button>
+
+              <Link to="forgot-password">Esqueci minha senha</Link>
+            </Form>
+
+            <Link to="signup">
+              <FiLogIn />
+              Criar conta
+            </Link>
+          </AnimationContainer>
+        </Content>
+        <Background />
+      </Container>
+    </>
+  );
+};
+
+export default SignIn;
